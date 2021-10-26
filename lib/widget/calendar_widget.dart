ac364a459e3c389834b2c0fcce1c6d2ff3fc7184
aleale2122 <alefew.ymer@jithvar.com>
2021-10-26 10:43:18 +0300

    first commit

---
+++ lib/widget/calendar_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+// import 'package:pro';
+
+class CalendarWidget extends StatelessWidget {
+  const CalendarWidget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCalendar(
+
+    );
+  }
+}
